cb85b8f8d9c0e837479d5afe71870721ea90873d
Lucas <lucas.gonthier@icloud.com>
2020-03-02 23:48:01 +0100

    prepare to self-references relationship

---
+++ app/helpers/permission_helper.rb
@@ -0,0 +1,10 @@
+module PermissionHelper
+
+    def canEdit(record)
+        if record.respond_to?(:user_id) and is_user_signed_in and record.user_id === current_user.id
+            true
+        else
+            false
+        end
+    end
+end
