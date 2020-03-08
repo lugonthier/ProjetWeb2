c548634366a0305626693a75a3f54fa00de0b49d
Lucas <lucas.gonthier@icloud.com>
2020-03-08 20:39:54 +0100

    heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'spoddict.herokuapp.com', :protocol => 'http' }
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
